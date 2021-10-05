cd28bcb02e548baa0412419b544fb8d4b0e3ab8f
Andrew Syrmakesis <siarziss@gmail.com>
2021-10-05 14:10:18 +0300

    rtds tcp packet exchange (completed)

--- rtds-communication/tcp_client.py
+++ rtds-communication/tcp_client.py
@@ -1,4 +1,7 @@
 import socket
+import time
+import struct
+import binascii
 import yaml
 
 # variable to choose test TCP server or RTDS
@@ -18,23 +21,33 @@ else:
 
 print(IP, PORT)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s, open("frequency_measurements.txt", "w") as f:
 	s.connect((IP, PORT))
 
 	if test:
+		# Test server
+
+		# read test TCP server's reply
 		test_server_reply = s.recv(1024)
 		print(test_server_reply)
 	else:
-		for i in range(20):
-			# define which measurements should be received
-			s.send('frequency_float = MeterCapture("W2");')
-			s.send('sprintf(frequency_string, "W2 = %f END", frequency_float);')
-			s.send('ListenOnPortHandshake(frequency_string);')
+		# RTDS server
+		
+		# command that sends data to RTDS
+		# s.sendall((1).to_bytes(4, 'big'))
+		
+		# while(True):
+		for i in range(5):
+			ang_velocity = s.recv(4)
+			
+			# convert received data point from bytes to hex
+			ang_velocity_hex = binascii.hexlify(ang_velocity)
+			
+			# convert hexademical data point to float
+			ang_velocity_float = struct.unpack('>f', binascii.unhexlify(ang_velocity_hex))[0]
+			frequency = ang_velocity_float / (2 * 3.14159265359)
+			print(frequency)
 
-			# get measurements
-			tokenstring = s.recv(1024)
-			print('Frequency value at time step ', i, ' is: ', tokenstring)
-
-			# following command is sent to RTDS TCP server to stop
-			# for 1.0 seconds
-			s.send('SUSPEND 1.0;')
+			f.write(str(frequency) + '\n')
+			
+			time.sleep(1)
