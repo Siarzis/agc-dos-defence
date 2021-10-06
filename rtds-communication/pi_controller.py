72f1a1ab4ccf5d02ffa34ca23e9754843967ec3d
Siazis <andrew.d.syrmakesis@gmail.com>
2021-10-06 19:14:44 +0300

    PI controller (fixed)

--- rtds-communication/pi_controller.py
+++ rtds-communication/pi_controller.py
@@ -2,31 +2,30 @@ import time
 
 class PI:
 	def __init__(self, Kp, Ki, setpoint):
-		self.e = 0
-		self.prev_t = -1
+		self.prev_t = -1.0
 		self.I = 0
 		self.Kp = Kp
 		self.Ki = Ki
 		self.SP = setpoint
 
-	def calculate(self, current_timestep, process_value):
+	def calculate_error(self, current_timestep, process_value):
 		t, PV = current_timestep, process_value
 		
-		self.e = self.SP - PV
+		e = self.SP - PV
 
 		P = self.Kp * self.e
 		self.I = self.I + self.Ki * self.e * (t - self.prev_t)
 
-		new_PV = P + self.I
+		self.prev_t = t
 		
-		return new_PV
+		return P + self.I
 
 
 """ if __name__ == '__main__':
 	process_value = 0
 	setpoint = 20
 
-	Kp = 2
+	Kp = 0.5
 	Ki = 0.1
 
 	pi = PI(Kp, Ki, setpoint)
@@ -34,7 +33,7 @@ class PI:
 	print(process_value, setpoint)
 
 	for i in range(1000):
-		process_value = pi.calculate(i, process_value)
+		process_value = pi.calculate_error(i, process_value)
 
 		print(process_value, setpoint)
 		# time.sleep(1) """
