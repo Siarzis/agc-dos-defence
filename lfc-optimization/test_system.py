4101d56c98385dee707b3b4b0ec94995541bc047
Siazis <andrew.d.syrmakesis@gmail.com>
2022-01-24 12:33:44 +0200

    create fitness function for dummy system

--- lfc-optimization/test_system.py
+++ lfc-optimization/test_system.py
@@ -1,13 +1,27 @@
-import control as ct
+from control import *
+from scipy.integrate import simpson
 import matplotlib.pyplot as plt
 
-s = ct.tf('s')
 
-Kp, Ki, Kd = 10, 100, 50
 
-plant = 1 / (s**2 + 10*s + 20)
-controller = Kp + Ki/s + Kd*s
+def pid_optimizer(x):
+	s = tf('s')
+	
+	Kp, Ki, Kd = x[0], x[1], x[2]
+	
+	plant = 1 / (s**2 + 10*s + 20)
+	controller = Kp + Ki/s + Kd*s
+	
+	error = abs(0 - step_response(feedback(plant, controller)).outputs)
 
-response = ct.step_response(ct.feedback(plant, controller))
-plt.plot(response.time, response.outputs)
-plt.show()
+	# TODO GA parameter: error criterion
+	J = simpson(error)
+
+	return J
+	
+	# plt.plot(response.time, response.outputs)
+	# plt.show()
+
+Kp, Ki, Kd = 100, 200, 50
+
+pid_optimizer([Kp, Ki, Kd])
