9c60c0fa22d6316765dd90bc8c5e84fcb88d50b2
Siazis <andrew.d.syrmakesis@gmail.com>
2022-01-27 17:19:51 +0200

    two area load frequency control state space implementation [not working]

--- lfc-optimization/two_area_lfc.py
+++ lfc-optimization/two_area_lfc.py
@@ -5,17 +5,68 @@ import matplotlib.pyplot as plt
 # is defined by its State Space representation
 
 # Parameters
-Kps1, Tps1 = 1, 20
-Kg1, Tg1 = 1, 0.8
-Kt1, Tt1 = 1, 0.3
+Kps1, Tps1 = 120, 20
+Kg1, Tg1 = 1, 0.08
+Kt1, Tt1 = 1, 0.5
 R1 = 2.4
+B1 = 0.425
 
-Kps1, Tps1 = 1, 20
-Kg2, Tg2 = 1, 0.8
-Kt2, Tt2 = 1, 0.3
+Kps2, Tps2 = 120, 20
+Kg2, Tg2 = 1, 0.08
+Kt2, Tt2 = 1, 0.5
 R2 = 2.4
+B2 = 0.425
 
-A = [[0, 1.], [-k/m, -b/m]]
-B = [[0], [1/m]]
-C = [[1., 0]]
-sys = ss(A, B, C, 0)
+T12 = 0.215
+a12 = -1
+
+A = [[-1/Tps1, Kps1/Tps1, 0, 0, 0, 0, -Kps1/Tps1, 0, 0],
+	 [0, -1/Tt1, 1/Tt1, 0, 0, 0, 0, 0, 0],
+	 [-1/(R1/Tg1), 0, -1/Tg1, 0, 0, 0, 0, 0, 0],
+	 [0, 0, 0, -1/Tps2, Kps2/Tps2, 0, -a12*Kps2/Tps2, 0, 0],
+	 [0, 0, 0, 0, -1/Tt2, 1/Tt2, 0, 0, 0],
+	 [0, 0, 0, -1/(R2/Tg2), 0, -1/Tg2, 0, 0, 0],
+	 [T12, 0, 0, -T12, 0, 0, 0, 0, 0],
+	 [B1, 0, 0, 0, 0, 0, 1, 0, 0],
+	 [0, 0, 0, B2, 0, 0, a12, 0, 0]
+	]
+
+B = [[0, 0],
+	 [0, 0],
+	 [Tg1, 0],
+	 [0, 0],
+	 [0, 0],
+	 [0, Tg2],
+	 [0, 0],
+	 [0, 0],
+	 [0, 0]
+	]
+
+F = [[-Kps1/Tps1, 0],
+	 [0, 0],
+	 [0, 0],
+	 [0, Kps2/Tps2],
+	 [0, 0],
+	 [0, 0],
+	 [0, 0],
+	 [0, 0],
+	 [0, 0]
+	]
+
+BF = np.concatenate([B, F], axis=1).tolist()
+
+print(BF)
+
+C = [[1.0, 0, 0, 0, 0, 0, 0, 0, 0],
+	 [0, 0, 0, 1.0, 0, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 0, 0, 0, 1.0, 0],
+	 [0, 0, 0, 0, 0, 0, 0, 0, 1.0]
+	]
+
+two_area = ss(A, BF, C, 0)
+
+response = step_response(two_area)
+
+plt.plot(response.time, response.outputs)
+
+plt.show()
