8b571cef3ebc3948a3e61343195f9b22531e694a
Siazis <andrew.d.syrmakesis@gmail.com>
2022-02-02 15:51:52 +0200

    two area lfc with distrurbance defined by state space [not working]

--- lfc-optimization/two_area_lfc.py
+++ lfc-optimization/two_area_lfc.py
@@ -22,10 +22,10 @@ a12 = -1
 
 A = [[-1/Tps1, Kps1/Tps1, 0, 0, 0, 0, -Kps1/Tps1, 0, 0],
 	 [0, -1/Tt1, 1/Tt1, 0, 0, 0, 0, 0, 0],
-	 [-1/(R1/Tg1), 0, -1/Tg1, 0, 0, 0, 0, 0, 0],
+	 [-1/(R1*Tg1), 0, -1/Tg1, 0, 0, 0, 0, 0, 0],
 	 [0, 0, 0, -1/Tps2, Kps2/Tps2, 0, -a12*Kps2/Tps2, 0, 0],
 	 [0, 0, 0, 0, -1/Tt2, 1/Tt2, 0, 0, 0],
-	 [0, 0, 0, -1/(R2/Tg2), 0, -1/Tg2, 0, 0, 0],
+	 [0, 0, 0, -1/(R2*Tg2), 0, -1/Tg2, 0, 0, 0],
 	 [T12, 0, 0, -T12, 0, 0, 0, 0, 0],
 	 [B1, 0, 0, 0, 0, 0, 1, 0, 0],
 	 [0, 0, 0, B2, 0, 0, a12, 0, 0]
@@ -33,10 +33,10 @@ A = [[-1/Tps1, Kps1/Tps1, 0, 0, 0, 0, -Kps1/Tps1, 0, 0],
 
 B = [[0, 0],
 	 [0, 0],
-	 [Tg1, 0],
+	 [1/Tg1, 0],
 	 [0, 0],
 	 [0, 0],
-	 [0, Tg2],
+	 [0, 1/Tg2],
 	 [0, 0],
 	 [0, 0],
 	 [0, 0]
@@ -45,7 +45,7 @@ B = [[0, 0],
 F = [[-Kps1/Tps1, 0],
 	 [0, 0],
 	 [0, 0],
-	 [0, Kps2/Tps2],
+	 [0, -Kps2/Tps2],
 	 [0, 0],
 	 [0, 0],
 	 [0, 0],
@@ -53,7 +53,7 @@ F = [[-Kps1/Tps1, 0],
 	 [0, 0]
 	]
 
-BF = np.concatenate([B, F], axis=1).tolist()
+BF = np.hstack([B, F]).tolist()
 
 C = [[1.0, 0, 0, 0, 0, 0, 0, 0, 0],
 	 [0, 0, 0, 1.0, 0, 0, 0, 0, 0],
@@ -61,12 +61,26 @@ C = [[1.0, 0, 0, 0, 0, 0, 0, 0, 0],
 	 [0, 0, 0, 0, 0, 0, 0, 0, 1.0]
 	]
 
-two_area = ss(A, BF, C, 0)
+two_area_lfc = ss(A, BF, C, 0)
 
-response = step_response(two_area)
+T = np.arange(0, 30, 0.1, dtype=float).tolist()
 
-print(response.outputs.shape)
+U = [np.zeros_like(T).tolist(), np.zeros_like(T).tolist(), np.ones_like(T).tolist(), np.zeros_like(T).tolist()]
 
-plt.plot(response.time, response.outputs[0][2])
+t, y = forced_response(two_area_lfc, T=T, U=U)
+
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=True)
+
+ax1.plot(t, y[0])
+ax1.set_title('Output 1')
+
+ax2.plot(t, y[1])
+ax2.set_title('Output 2')
+
+ax3.plot(t, y[2])
+ax3.set_title('Output 3')
+
+ax4.plot(t, y[3])
+ax4.set_title('Output 4')
 
 plt.show()
