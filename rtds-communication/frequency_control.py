814ae1c43285775705e5c6b914b42d2382896d15
Siazis <andrew.d.syrmakesis@gmail.com>
2022-02-11 16:34:43 +0200

    deleted the test TCP server [no usage]

--- rtds-communication/frequency_control.py
+++ rtds-communication/frequency_control.py
@@ -6,20 +6,13 @@ import yaml
 
 from simple_pid import PID
 
-# variable to choose test TCP server or RTDS TCP server
-test = False
-
-if test:
-	IP = '127.0.0.1'
-	PORT = 65432
-else:
-	with open('rtds_server.yaml', 'r') as stream:
-		try:
-			rtds_credentials = yaml.safe_load(stream)
-			IP = rtds_credentials.get('ADDRESS')['IP']
-			PORT = rtds_credentials.get('ADDRESS')['PORT']
-		except yaml.YAMLError as exc:
-			print(exc)
+with open('rtds_server.yaml', 'r') as stream:
+	try:
+		rtds_credentials = yaml.safe_load(stream)
+		IP = rtds_credentials.get('ADDRESS')['IP']
+		PORT = rtds_credentials.get('ADDRESS')['PORT']
+	except yaml.YAMLError as exc:
+		print(exc)
 
 Kp = 0.0
 Ki = 0.001
@@ -32,42 +25,33 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s, open("frequency_mea
 
 	pi = PID(Kp, Ki, Kd, setpoint=setpoint)
 
-	if test:
-		# Test server
+	previous_time = time.time_ns() / (10 ** 9)
 
-		# read test TCP server's reply
-		test_server_reply = s.recv(1024)
-		print(test_server_reply)
-	else:
-		# RTDS server
-		
-		previous_time = time.time_ns() / (10 ** 9)
-
-		while(True):
-			ang_velocity = s.recv(4)
-			power_gen1 = s.recv(4)
+	while(True):
+		ang_velocity = s.recv(4)
+		power_gen1 = s.recv(4)
 			
-			# convert received data point from bytes to hex
-			ang_velocity_hex = binascii.hexlify(ang_velocity)
-			power_gen1_hex = binascii.hexlify(power_gen1)
+		# convert received data point from bytes to hex
+		ang_velocity_hex = binascii.hexlify(ang_velocity)
+		power_gen1_hex = binascii.hexlify(power_gen1)
 			
-			# convert hexademical data point to float
-			ang_velocity_float = struct.unpack('>f', binascii.unhexlify(ang_velocity_hex))[0]
-			power_gen1_float = struct.unpack('>f', binascii.unhexlify(power_gen1_hex))[0]
+		# convert hexademical data point to float
+		ang_velocity_float = struct.unpack('>f', binascii.unhexlify(ang_velocity_hex))[0]
+		power_gen1_float = struct.unpack('>f', binascii.unhexlify(power_gen1_hex))[0]
 
-			# select sampling time
-			current_time = time.time_ns() / (10 ** 9)
-			time_diff = current_time - previous_time
+		# select sampling time
+		current_time = time.time_ns() / (10 ** 9)
+		time_diff = current_time - previous_time
 
-			if time_diff > 0.101:
-				previous_time = current_time
+		if time_diff > 0.101:
+			previous_time = current_time
 
-				# compute Area Control Error (ACE)
-				ace_float = pi(ang_velocity_float)
-				ace = bytearray(struct.pack('>f', ace_float))
+			# compute Area Control Error (ACE)
+			ace_float = pi(ang_velocity_float)
+			ace = bytearray(struct.pack('>f', ace_float))
+			
+			print(power_gen1_float, ang_velocity_float, ace_float)
+			s.sendall(ace)
 
-				print(power_gen1_float, ang_velocity_float, ace_float)
-				s.sendall(ace)
-
-				frequency = ang_velocity_float / (2 * 3.14159265359)
-				f.write(str(power_gen1_float) + ', ' + str(ang_velocity_float) + ', ' + str(ace_float) + '\n')
+			frequency = ang_velocity_float / (2 * 3.14159265359)
+			f.write(str(power_gen1_float) + ', ' + str(ang_velocity_float) + ', ' + str(ace_float) + '\n')
